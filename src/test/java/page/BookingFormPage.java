e446849edfca76d3d2e0517f63668b63907acaef
KozlovskyS <skozmail@gmail.com>
2026-05-03 11:30:29 +0300

    find input - success

--- src/test/java/page/BookingFormPage.java
+++ src/test/java/page/BookingFormPage.java
@@ -13,15 +13,34 @@ import static com.codeborne.selenide.Selenide.*;
 public class BookingFormPage {
     private final SelenideElement heading = $(withText("Заявка на бронирование стола"));
 
-    private  final SelenideElement nameField = $$("input")
-            .findBy(Condition.attribute("placeholder","например, Иван"));
+//    private  final SelenideElement nameField = $$("input")
+//            .findBy(Condition.attribute("placeholder","например, Иван"));
 
-//    private final SelenideElement nameField = $("label")
-//            .shouldHave(text("Ваше имя"))  // ждём, пока label будет содержать текст
-//            .closest("div")          // поднимаемся до ближайшего родительского div
-//            .find("input");          // ищем input внутри этого div
+//    private  final SelenideElement telefoneField = $$("input")
+//            .findBy(Condition.attribute("placeholder","Телефон"));
 
-    //SelenideElement nameField = $("input [placeholder='например, Иван']");
+    private final SelenideElement nameField = $$("label")
+            .findBy(text("Ваше имя"))  // ждём, пока label будет содержать текст
+            .closest("div")          // поднимаемся до ближайшего родительского div
+            .find("input");          // ищем input внутри этого div
+    private  final SelenideElement phoneField = $$("label")
+            .findBy(text("Номер телефона"))  // ждём, пока label будет содержать текст
+            .closest("div")          // поднимаемся до ближайшего родительского div
+            .find("input");          // ищем input внутри этого div
+
+    private  final SelenideElement guestField = $$("label")
+            .findBy(text("Количество гостей"))
+            .closest("div")
+            .find("input");
+    private  final SelenideElement wishesField = $$("label")
+            .findBy(text("Ваши Пожелания"))
+            .closest("div")
+            .find("textarea");
+    private  final SelenideElement dateField = $$("label")
+            .findBy(text("Дата посещения"))
+            .closest("div")
+            .find("input");
+    private final SelenideElement buttonSend = $$("button").get(0);
 
     public void BookingFormPage() {
         heading.shouldBe(visible);
@@ -31,6 +50,26 @@ public class BookingFormPage {
         nameField.shouldBe(visible, Duration.ofSeconds(10));
         nameField.sendKeys(name);
     }
-
-
+    public void setPhone(String telNumber) {
+        phoneField.shouldBe(visible, Duration.ofSeconds(10));
+        phoneField.sendKeys(telNumber);
+    }
+    public void setGuest(String number) {
+        guestField.shouldBe(visible, Duration.ofSeconds(10));
+        guestField.sendKeys(number);
+    }
+    public void setWishes(String wishes) {
+        wishesField.shouldBe(visible, Duration.ofSeconds(10));
+        wishesField.sendKeys(wishes);
+    }
+    public void clickSend() {
+        buttonSend.shouldBe(visible);
+        buttonSend.shouldBe(enabled);
+        buttonSend.click();
+    }
+    public void setDate(String date) {
+        dateField.shouldBe(visible);
+        dateField.click();
+        dateField.setValue(date);
+    }
 }
--- src/test/java/test/BookingFormTest.java
+++ src/test/java/test/BookingFormTest.java
@@ -2,6 +2,7 @@ package test;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.collections.Texts;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -12,19 +13,26 @@ import org.junit.jupiter.api.Test;
 import page.BookingFormPage;
 import page.MainPage;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class BookingFormTest {
+
+
     @BeforeAll
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
     @BeforeEach
     public void setUp() {
         //open("http://localhost:3000/");
         open("https://resto.skroy.ru/");
     }
+
     @AfterAll
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
@@ -33,8 +41,16 @@ public class BookingFormTest {
     @Test
     void successBooking() {                       // успешная отправка заявки
         MainPage mainPage = new MainPage();
-        var bookingFormPage= mainPage.choiceReservation();
-        bookingFormPage.setName("kaka");
+        var bookingFormPage = mainPage.choiceReservation();
+        switchTo().frame($("iframe"));
 
+        bookingFormPage.setName("kakaka");
+        bookingFormPage.setPhone("9991112233");
+        bookingFormPage.setGuest("4");
+        bookingFormPage.setWishes("werv vghhh 2345 %^^^");
+        bookingFormPage.setDate("15.05.2026");
+        bookingFormPage.clickSend();
+
+        switchTo().defaultContent();
     }
 }
